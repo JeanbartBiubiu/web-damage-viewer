0be3448831aff629eccebf2f2f0cd8d4aeb17f79
ml <haveno@qq.com>
2024-10-15 00:00:18 +0800

    feat: 添加echarts 3D示例

--- src/views/menu/menu2/index.vue
+++ src/views/menu/menu2/index.vue
@@ -1,17 +1,84 @@
+<template>
+  <div ref="chartContainer" style="width: 600px; height: 400px"></div>
+</template>
+
 <script lang="ts" setup>
-import { ref } from "vue"
+import { ref, onMounted } from "vue"
+import * as echarts from "echarts"
+import 'echarts-gl';
 
-defineOptions({
-  name: "Menu2"
+const chartContainer = ref<HTMLElement | null>(null)
+
+onMounted(() => {
+  if (chartContainer.value) {
+    const myChart = echarts.init(chartContainer.value)
+    const option = {
+      tooltip: {},
+      backgroundColor: "#fff",
+      visualMap: {
+        show: false,
+        dimension: 2,
+        min: -1,
+        max: 1,
+        inRange: {
+          color: [
+            "#313695",
+            "#4575b4",
+            "#74add1",
+            "#abd9e9",
+            "#e0f3f8",
+            "#ffffbf",
+            "#fee090",
+            "#fdae61",
+            "#f46d43",
+            "#d73027",
+            "#a50026"
+          ]
+        }
+      },
+      xAxis3D: {
+        type: "value"
+      },
+      yAxis3D: {
+        type: "value"
+      },
+      zAxis3D: {
+        type: "value"
+      },
+      grid3D: {
+        viewControl: {
+          // projection: 'orthographic'
+        }
+      },
+      series: [
+        {
+          type: "surface",
+          wireframe: {
+            // show: false
+          },
+          equation: {
+            x: {
+              step: 0.05
+            },
+            y: {
+              step: 0.05
+            },
+            z: function (x, y) {
+              if (Math.abs(x) < 0.1 && Math.abs(y) < 0.1) {
+                return "-"
+              }
+              return Math.sin(x * Math.PI) * Math.sin(y * Math.PI)
+            }
+          }
+        }
+      ]
+    }
+    myChart.setOption(option)
+    console.log("渲染咯")
+  }
 })
-
-const text = ref("")
 </script>
 
-<template>
-  <div class="app-container">
-    <el-card header="二级路由缓存 - menu2">
-      <el-input v-model="text" />
-    </el-card>
-  </div>
-</template>
+<style scoped>
+/* 你可以在这里添加任何必要的样式 */
+</style>
