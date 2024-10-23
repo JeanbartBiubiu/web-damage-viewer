f8227ac7696929133b44e4dc8f74a13a520ccd51
ml <haveno@qq.com>
2024-10-23 23:54:44 +0800

    第一个可以看的图

--- src/utils/chartExapmle.ts
+++ src/utils/chartExapmle.ts
@@ -6,6 +6,15 @@ export class IndivExample{
   ChuantouBaifenbi!: number;
   Baoji!: number;
 
+  constructor(attk:number,speed:number,hujia:number,chuantou:number,chuantoubaifenbi:number,baoji:number){
+    this.Attk = attk;
+    this.Speed = speed;
+    this.HuJia = hujia;
+    this.Chuantou = chuantou;
+    this.ChuantouBaifenbi = chuantoubaifenbi;
+    this.Baoji = baoji;
+  }
+
   Attack(attkObject:IndivExample){
     if (Math.random() >= this.Baoji){
       return this.Attk*(100/((100+((attkObject.HuJia-this.Chuantou)*(1-this.ChuantouBaifenbi))))) * 1.75
@@ -13,4 +22,13 @@ export class IndivExample{
       return this.Attk*(100/((100+((attkObject.HuJia-this.Chuantou)*(1-this.ChuantouBaifenbi)))))
     }
   }
+}
+
+export function AttackTime(timeSpeed:number,objA:IndivExample,ObjB:IndivExample){
+  let count = 0,sum = 0
+  for(;count <= timeSpeed;count+=1/objA.Speed) {
+    sum += objA.Attack(ObjB)
+
+  }
+  return sum
 }
--- src/views/menu/menu2/index.vue
+++ src/views/menu/menu2/index.vue
@@ -1,49 +1,50 @@
-<template>
-  <div class="app-container">
-    <el-card v-loading="loading" shadow="never" class="search-wrapper">
-      <el-form ref="IndivExampleA" :inline="true" :model="aObj">
-        <el-form-item prop="Attk" label="攻击力">
-          <el-input v-model="IndivExampleA.Attk" placeholder="请输入" />
-        </el-form-item>
-      </el-form>
-    </el-card>
-  </div>
-  <div class="app-container">
-    <el-card v-loading="loading" shadow="never" class="search-wrapper">
-      <el-form ref="IndivExampleB" :inline="true" :model="bObj">
-        <el-form-item prop="Attk" label="攻击力">
-          <el-input v-model="IndivExampleB.Attk" placeholder="请输入" />
-        </el-form-item>
-      </el-form>
-    </el-card>
-  </div>
-  <div ref="chartContainer" style="width: 800px; height: 800px;"></div>
-</template>
-
 <script lang="ts" setup>
-import { ref, onMounted } from "vue"
+import { ref, onMounted, reactive } from "vue"
 import * as echarts from "echarts"
-import {IndivExample} from "@/utils/chartExapmle"
+import {AttackTime, IndivExample} from "@/utils/chartExapmle"
 import 'echarts-gl';
+import { type FormInstance, type FormRules, ElMessage, ElMessageBox } from "element-plus"
 
 const chartContainer = ref<HTMLElement | null>(null)
 
-const IndivExampleA = ref<IndivExample>(new IndivExample())
-const IndivExampleB = ref<IndivExample>(new IndivExample())
-const aObj = new IndivExample()
-const bObj = new IndivExample()
+const IndivExampleA = ref<FormInstance | null>(null)
+const IndivExampleB = ref<FormInstance | null>(null)
+// 使用 reactive 创建响应式对象
+const aObj = reactive({
+  Attk: 350,
+  HuJia: 100,
+  Speed: 2.0,
+  Chuantou: 30,
+  ChuantouBaifenbi: 0.3,
+  Baoji: 1
+})
 
+const bObj = reactive({
+  Attk: 350,
+  HuJia: 211,
+  Speed: 1.3,
+  Chuantou: 0,
+  ChuantouBaifenbi: 0,
+  Baoji: 0
+})
 onMounted(() => {
+  updateChart()
+})
+
+function updateChart(){
+  console.log(chartContainer.value)
   if (chartContainer.value) {
+    let innerAObj = new IndivExample(aObj.Attk,aObj.Speed,aObj.HuJia,aObj.Chuantou,aObj.ChuantouBaifenbi,aObj.Baoji);
+    let innerBObj = new IndivExample(bObj.Attk,bObj.Speed,bObj.HuJia,bObj.Chuantou,bObj.ChuantouBaifenbi,bObj.Baoji);
     const myChart = echarts.init(chartContainer.value)
-    const option = {
+    var option = {
       tooltip: {},
       backgroundColor: "#fff",
       visualMap: {
-        show: false,
+        show: true,
         dimension: 2,
-        min: -100,
-        max: 100,
+        min: -1,
+        max: 1,
         inRange: {
           color: [
             "#313695",
@@ -61,13 +62,21 @@ onMounted(() => {
         }
       },
       xAxis3D: {
-        type: "value"
+        type: "value",
+        name: "攻击力",
+        min:0,
+        max: 600,
       },
       yAxis3D: {
-        type: "value"
+        type: "value",
+        name: "护甲",
+        min:0,
+        max: 500,
       },
       zAxis3D: {
-        type: "value"
+        type: "value",
+        name: "伤害",
+        min:0
       },
       grid3D: {
         viewControl: {
@@ -78,24 +87,32 @@ onMounted(() => {
         {
           type: "surface",
           wireframe: {
-            // show: false
+             show: true
           },
           equation: {
             x: {
-              step: 1
+              step: 10,
+              min:0,
+              max:600
             },
             y: {
-              step: 1
+              step: 15,
+              min:0,
+              max:500
             },
             z: function (x:number, y:number) {
-              aObj.Attk = x;
-              bObj.HuJia = y;
-              console.log("打印："+aObj.Attack(bObj))
-              if (Number.isNaN(aObj.Attack(bObj))){
-                console.log("wawawa")
-                return Math.random()*100
+              console.log("x=",x)
+              console.log("y=",y)
+              innerAObj.Attk = aObj.Attk + x;
+              innerBObj.HuJia = bObj.HuJia + y;
+              if (Number.isNaN(innerAObj.Attack(innerBObj))){
+                const d = Math.random()*100
+                console.log(d)
+                return d
               } else{
-                return aObj.Attack(bObj)
+                const d = AttackTime(10,innerAObj,innerBObj)
+                console.log(d)
+                return d
               }
             }
           }
@@ -103,10 +120,64 @@ onMounted(() => {
       ]
     }
     myChart.setOption(option)
-    console.log("渲染咯")
   }
-})
+}
 </script>
+
+<template>
+  <div class="app-container">
+    <el-card shadow="never" class="search-wrapper">
+      <el-form ref="IndivExampleA" :inline="true" :model="aObj">
+        <el-form-item prop="Attk" label="攻击力">
+          <el-input v-model="aObj.Attk" placeholder="请输入" />
+        </el-form-item>
+        <el-form-item prop="Hujia" label="护甲">
+          <el-input v-model="aObj.HuJia" placeholder="请输入" />
+        </el-form-item>
+        <el-form-item prop="Speed" label="攻速">
+          <el-input v-model="aObj.Speed" placeholder="请输入" />
+        </el-form-item>
+        <el-form-item prop="Chuantou" label="穿甲">
+          <el-input v-model="aObj.Chuantou" placeholder="请输入" />
+        </el-form-item>
+        <el-form-item prop="ChuantouBaifenbi" label="穿甲百分比">
+          <el-input v-model="aObj.ChuantouBaifenbi" placeholder="请输入" />
+        </el-form-item>
+        <el-form-item prop="Baoji" label="暴击几率">
+          <el-input v-model="aObj.Baoji" placeholder="请输入" />
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
+  <div class="app-container">
+    <el-card shadow="never" class="search-wrapper">
+      <el-form ref="IndivExampleB" :inline="true" :model="bObj">
+        <el-form-item prop="Attk" label="攻击力">
+          <el-input v-model="bObj.Attk" placeholder="请输入" />
+        </el-form-item>
+        <el-form-item prop="Hujia" label="护甲">
+          <el-input v-model="bObj.HuJia" placeholder="请输入" />
+        </el-form-item>
+        <el-form-item prop="Speed" label="攻速">
+          <el-input v-model="bObj.Speed" placeholder="请输入" />
+        </el-form-item>
+        <el-form-item prop="Chuantou" label="穿甲">
+          <el-input v-model="bObj.Chuantou" placeholder="请输入" />
+        </el-form-item>
+        <el-form-item prop="ChuantouBaifenbi" label="穿甲百分比">
+          <el-input v-model="bObj.ChuantouBaifenbi" placeholder="请输入" />
+        </el-form-item>
+        <el-form-item prop="Baoji" label="暴击几率">
+          <el-input v-model="bObj.Baoji" placeholder="请输入" />
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
+  <div class="app-container">
+    <el-button @click="updateChart">更新图表</el-button>
+  </div>
+  <div ref="chartContainer" style="width: 1000px; height: 1000px;"></div>
+</template>
 
 <style scoped>
 /* 你可以在这里添加任何必要的样式 */
