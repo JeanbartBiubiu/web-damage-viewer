b305cee601e075c9195200ea5623b035191b649d
ml <haveno@qq.com>
2025-04-11 21:55:02 +0800

    ga

--- src/layouts/components/Footer/index.vue
+++ src/layouts/components/Footer/index.vue
@@ -8,8 +8,10 @@ const VITE_APP_TITLE = import.meta.env.VITE_APP_TITLE
       <a href="https://beian.miit.gov.cn/" target="_blank">粤ICP备2025393185号-1</a>
     </p>
     <p>
-      <img src="https://beian.mps.gov.cn/img/logo01.dd7ff50e.png" width="20"/>
-      <a href="https://beian.mps.gov.cn/#/query/webSearch?code=1111111" rel="noreferrer" target="_blank">粤公网安备1111111111号</a>
+      <img src="https://beian.mps.gov.cn/img/logo01.dd7ff50e.png" width="20" />
+      <a href="https://beian.mps.gov.cn/#/query/webSearch?code=1111111" rel="noreferrer" target="_blank"
+        >粤公网安备44030002006529号</a
+      >
     </p>
   </footer>
 </template>
